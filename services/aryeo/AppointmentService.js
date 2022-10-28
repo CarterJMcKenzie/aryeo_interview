7c54b1cc2f981d43fc557e6def8e056fb69e4660
Jarrod Parkes <parkesfjarrod@gmail.com>
2022-10-28 11:31:48 -0500

    add optional params to appts index

--- services/aryeo/AppointmentService.js
+++ services/aryeo/AppointmentService.js
@@ -13,34 +13,41 @@ async function appointments() {
     return await aryeoRequest({
         url: `/appointments`,
         method: "GET",
-        headers: authHeaders(),
-    });
-}
-
-async function appointmentCancel(appointment_id, notify_customer) {
-    return await aryeoRequest({
-        url: `/appointments/${appointment_id}/cancel`,
-        method: "PUT",
-        data: {
-            notify_customer: notify_customer,
+        // Optional parameters...
+        params: {
+            "filter[search]": "", // (string) text search for appointment address or customer
+            "filter[statuses][]": "", // (string, mulitple) possible values: SCHEDULED, UNSCHEDULE, CANCELED, DRAFT
+            "filter[statuses][]": "", // (string, multiple) possible values: SCHEDULED, UNSCHEDULE, CANCELED, DRAFT
+            "filter[tense]": "UPCOMING", // (string) possible values: PAST, UPCOMING
+            "filter[start_at_gte]": "2022-03-12T22:31:21Z", // (date string)
+            "filter[start_at_lte]": "2022-03-12T22:31:21Z", // (date string)
+            "sort": "-start_timestamp,-created_at", // (string) comma-separated listed with -start_timestamp or -created_at
+            "per_page": 25, // (int)
+            "page": 1, // (int)
         },
         headers: authHeaders(),
     });
 }
 
-async function appointmentReschedule(
-    appointment_id,
-    start_at,
-    end_at,
-    notify_customer
-) {
+async function appointmentCancel(appointment_id) {
+    return await aryeoRequest({
+        url: `/appointments/${appointment_id}/cancel`,
+        method: "PUT",
+        data: {
+            notify_customer: true,
+        },
+        headers: authHeaders(),
+    });
+}
+
+async function appointmentReschedule(appointment_id) {
     return await aryeoRequest({
         url: `/appointments/${appointment_id}/schedule`,
         method: "PUT",
         data: {
-            start_at: start_at,
-            end_at: end_at,
-            notify_customer: notify_customer,
+            start_at: "2022-03-12T22:31:21Z",
+            end_at: "2022-03-12T24:31:21Z",
+            notify_customer: true,
         },
         headers: authHeaders(),
     });
